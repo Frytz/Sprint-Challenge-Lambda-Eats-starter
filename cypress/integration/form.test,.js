82de9548c438f25f44d94aa6c3e7bf2de94346dd
John Rossi <DACOOK1983@gamil.com>
2020-05-04 23:43:47 -0400

    code clean up

--- cypress/integration/form.test,.js
+++ cypress/integration/form.test,.js
@@ -7,6 +7,26 @@ describe("", function (){
         cy.get('[data-cy= nameArea]')
             .type("john")
             .should("have.value", "john");
-    })
+        cy.get('[data-cy= size]')
+            .select('18in')
+            .should('have.value', '18in')
+        cy.get('[data-cy= sauce]')
+            .select('marinara')
+            .should('have.value', 'marinara')
+        cy.get('[data-cy= pepperoni]').check()
+            .should('be.checked')
+        cy.get('[data-cy= sausage]').check()
+            .should('be.checked')
+        cy.get('[data-cy= canadian]').check()
+            .should('be.checked')
+        cy.get('[data-cy= spicy]').check()
+            .should('be.checked')
+        cy.get('[data-cy= chicken]').check()
+            .should('be.checked')
+        cy.get('[data-cy= addButton]').click()
+        cy.get('[data-cy= instructions]')
+            .type("test")
+            .should("have.value", "test");
+    });
 
 })
